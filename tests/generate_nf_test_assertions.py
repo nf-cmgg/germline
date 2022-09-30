f4eafc86ab430980199e55da4e8f1063d34229ce
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2022-09-30 17:32:36 +0200

    updated all tests

--- tests/generate_nf_test_assertions.py
+++ tests/generate_nf_test_assertions.py
@@ -1,5 +1,6 @@
 import argparse
 import glob
+import gzip
 import hashlib
 import os
 import re
@@ -14,9 +15,28 @@ if __name__ == "__main__":
     test_dir = args.test_dir
     all_outputs = glob.glob(f'{test_dir}/**/output/**', recursive=True)
 
+    tab = "\\t"
+
+    print("assert workflow.success")
+
     for output in all_outputs:
         abs_path = os.path.abspath(output)
         if os.path.isfile(abs_path):
-            md5sum = hashlib.md5(open(abs_path, 'rb').read()).hexdigest()
             file_name = re.search('^.*/output/(.*)$', output).group(1)
-            print(f"assert path(\"${{outputDir}}/{file_name}\").md5 == \"{md5sum}\"")
+            if re.search("^.*\.tbi$", output):
+                print(f"assert file(\"${{outputDir}}/{file_name}\").exists()")
+            elif re.search("^.*\.vcf.gz$", output):
+                with gzip.open(abs_path, 'rt') as vcf:
+                    for line in vcf:
+                        if re.search("^chr.*$", line):
+                            print(f"assert path(\"${{outputDir}}/{file_name}\").linesGzip.contains(\"{tab.join(line.split()).strip()}\")")
+                            break
+            elif re.search("^.*\.vcf$", output):
+                with open(abs_path, 'r') as vcf:
+                    for line in vcf:
+                        if re.search("^chr.*$", line):
+                            print(f"assert path(\"${{outputDir}}/{file_name}\").text.contains(\"{tab.join(line.split()).strip()}\")")
+                            break
+            else:
+                md5sum = hashlib.md5(open(abs_path, 'rb').read()).hexdigest()
+                print(f"assert path(\"${{outputDir}}/{file_name}\").md5 == \"{md5sum}\"")
